6074c147189dd0e5199a531672ae129ae8c6a41e
Anderson Bavalheri <andersonbravalheri@gmail.com>
2016-03-07 13:49:30 -0300

    chore(pyscaffold): add pyscaffold generated struct

---
+++ AUTHORS.rst
@@ -0,0 +1,5 @@
+==========
+Developers
+==========
+
+* Anderson Bravalheri <andersonbravalheri@gmail.com>
---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# isort:skip_file
+
+"""
+    Dummy conftest.py for pypiple.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+from __future__ import print_function, absolute_import, division
+
+import pytest
